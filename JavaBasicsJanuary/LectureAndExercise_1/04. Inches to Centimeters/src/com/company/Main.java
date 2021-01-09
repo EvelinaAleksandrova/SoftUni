4cf3285d9ead612de3adb5e7c67f752b19d0bad7
Evelina <evety.1dtvd@gmail.com>
2021-01-09 23:19:25 +0200

    Inches to Centimeters

---
+++ JavaBasicsJanuary/LectureAndExercise_1/04. Inches to Centimeters/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	String input = scanner.nextLine();
+
+	double inches = Double.parseDouble(input);
+	double centimeters = 2.54;
+	System.out.println(inches * 2.54);
+    }
+}
