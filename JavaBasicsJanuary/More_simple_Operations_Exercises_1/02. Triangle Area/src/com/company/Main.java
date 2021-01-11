193dca92e671673971aff8fcbfac59c8465f7d55
Evelina <evety.1dtvd@gmail.com>
2021-01-11 15:26:35 +0200

    Triangle Area

---
+++ JavaBasicsJanuary/More_simple_Operations_Exercises_1/02. Triangle Area/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double side = Double.parseDouble(scanner.nextLine());
+        double height = Double.parseDouble(scanner.nextLine());
+
+        double area = side * height / 2;
+        System.out.printf("%.2f",area);
+    }
+}
