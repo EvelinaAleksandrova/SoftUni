8353f2024cd7f8779434ac9d9d5dec9a08b3cdee
Evelina <evety.1dtvd@gmail.com>
2021-01-30 00:38:20 +0200

    Fruit Shop

--- JavaBasicsJanuary/Lecture_3/11. Fruit Shop/src/com/company/Main.java
+++ JavaBasicsJanuary/Lecture_3/11. Fruit Shop/src/com/company/Main.java
@@ -14,55 +14,57 @@ public class Main {
         if (dayOfWeek.equals("Monday") || dayOfWeek.equals("Tuesday") || dayOfWeek.equals("Wednesday") || dayOfWeek.equals("Thursday") || dayOfWeek.equals("Friday")) {
             if (product.equals("banana")) {
                 price = amount * 2.50;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("apple")) {
                 price = amount * 1.20;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("orange")) {
                 price = amount * 0.85;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("grapefruit")) {
                 price = amount * 1.45;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("kiwi")) {
                 price = amount * 2.70;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("pineapple")) {
                 price = amount * 5.50;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("grapes")) {
                 price = amount * 3.85;
-                System.out.printf("%.2f", price);
+
             } else {
                 System.out.println("error");
             }
         } else if (dayOfWeek.equals("Saturday") || dayOfWeek.equals("Sunday")) {
             if (product.equals("banana")) {
                 price = amount * 2.70;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("apple")) {
                 price = amount * 1.25;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("orange")) {
                 price = amount * 0.90;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("grapefruit")) {
                 price = amount * 1.60;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("kiwi")) {
                 price = amount * 3.00;
-                System.out.printf("%.2f", price);
+
             } else if (product.equals("pineapple")) {
                 price = amount * 5.60;
-                System.out.printf("%.2f",price);
+
             } else if (product.equals("grapes")) {
                 price = amount * 4.20;
-                System.out.printf("%.2f", price);
             } else {
                 System.out.println("error");
             }
         } else {
             System.out.println("error");
+        }
+        if (price > 0) {
+            System.out.printf("%.2f", price);
         }
     }
 }
