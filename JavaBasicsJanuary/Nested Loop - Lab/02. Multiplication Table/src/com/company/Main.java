173118c6e48ac0e9aca1098c1a60d8c65cac50df
Evelina <evety.1dtvd@gmail.com>
2021-02-15 18:27:31 +0200

    Multiplication Table

---
+++ JavaBasicsJanuary/Nested Loop - Lab/02. Multiplication Table/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int firstNumber = 1; firstNumber <= 10; firstNumber++) {
+            for (int secondNumber = 1; secondNumber <= 10; secondNumber++) {
+                System.out.println(firstNumber + " * " + secondNumber + " = " + firstNumber * secondNumber);
+            }
+        }
+    }
+}
