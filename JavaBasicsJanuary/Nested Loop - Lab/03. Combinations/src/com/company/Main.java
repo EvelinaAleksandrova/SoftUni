2b066178b675b6fd29e47c46708452dfa45184a3
Evelina <evety.1dtvd@gmail.com>
2021-02-15 20:37:13 +0200

    Combinations

---
+++ JavaBasicsJanuary/Nested Loop - Lab/03. Combinations/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int counter = 0;
+        //x1 + x2 + x3 = n
+        for (int x1 = 0; x1 <= number; x1++) {
+            for (int x2 = 0; x2 <= number; x2++) {
+                for (int x3 = 0; x3 <= number; x3++) {
+                    if(x1 + x2 + x3 == number){
+                        counter++;
+                    }
+                }
+            }
+        }
+        System.out.println(counter);
+    }
+}
