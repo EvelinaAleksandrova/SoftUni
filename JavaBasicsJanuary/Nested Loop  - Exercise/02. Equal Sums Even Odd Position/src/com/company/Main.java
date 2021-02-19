55e39607ffe2eb59550a13f3955c925d1ad2ef77
Evelina <evety.1dtvd@gmail.com>
2021-02-19 18:44:45 +0200

    Equal Sums Even Odd Position

---
+++ JavaBasicsJanuary/Nested Loop  - Exercise/02. Equal Sums Even Odd Position/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+
+        for (int i = firstNumber; i <= secondNumber; i++) {
+            String currentNumber = "" + i;
+            int sumEven = 0;
+            int sumOdd = 0;
+            for (int j = 0; j < currentNumber.length(); j++) {
+
+                int currentDigit = Integer.parseInt("" + currentNumber.charAt(j));
+
+                if (j % 2 == 0) {
+                    sumEven = sumEven + currentDigit;
+                }else{
+                    sumOdd = sumOdd + currentDigit;
+                }
+            }
+            if(sumEven == sumOdd){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
