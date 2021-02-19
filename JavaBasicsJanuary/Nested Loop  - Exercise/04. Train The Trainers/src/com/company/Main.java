e6a4ea74e565771c75f1deeed0711c0450a8b87f
Evelina <evety.1dtvd@gmail.com>
2021-02-20 00:13:43 +0200

    Train The Trainers

--- JavaBasicsJanuary/Nested Loop  - Exercise/04. Train The Trainers/src/com/company/Main.java
+++ JavaBasicsJanuary/Nested Loop  - Exercise/04. Train The Trainers/src/com/company/Main.java
@@ -11,7 +11,6 @@ public class Main {
         int counterGrades = 0;
         double grades = 0;
 
-
         while (true) {
             String nameOfPresentation = scanner.nextLine();
             double sumGrade = 0;
@@ -21,19 +20,14 @@ public class Main {
                 System.out.printf("Student's final assessment is %.2f.", totalAverage);
                 break;
             }
-
-
             for (int i = 0; i < numberOfJury; i++) {
                 double grade = Double.parseDouble(scanner.nextLine());
                 sumGrade += grade;
                 grades += grade;
                 counterGrades++;
             }
-
-double averageGrade = sumGrade / numberOfJury;
-            System.out.printf("%s - %.2f.%n",nameOfPresentation,averageGrade);
+            double averageGrade = sumGrade / numberOfJury;
+            System.out.printf("%s - %.2f.%n", nameOfPresentation, averageGrade);
         }
-
-
     }
 }
